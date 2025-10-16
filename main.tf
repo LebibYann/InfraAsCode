bdc0d64e1b02909a28d506ffa19dde7076b78534
Lebib <yann.lebib@epitech.eu>
2025-10-16 10:57:19 +0200

    feat(terraform): add Cloud Router and Cloud NAT resources

--- main.tf
+++ main.tf
@@ -98,6 +98,45 @@ resource "google_storage_bucket" "demo" {
   ]
 }
 
+# -----------------------------
+# Cloud Router
+# -----------------------------
+
+resource "google_compute_router" "nat_router" {
+  name    = "nat-router"
+  region  = var.region
+  network = google_compute_network.vpc_network.id
+
+  depends_on = [
+    google_compute_subnetwork.private_subnet
+  ]
+}
+
+# -----------------------------
+# Cloud NAT
+# -----------------------------
+
+resource "google_compute_router_nat" "nat_config" {
+  name                                = "nat-config"
+  router                              = google_compute_router.nat_router.name
+  region                              = var.region
+  nat_ip_allocate_option              = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat  = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name = google_compute_subnetwork.private_subnet.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+
+  depends_on = [
+    google_compute_router.nat_router
+  ]
+}
 
 # -----------------------------
 # Outputs
